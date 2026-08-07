b52b0c835193aafb99f527ef89f573b1dcd9026b
BilalRifas <bilal.rifas@gmail.com>
2026-08-08 09:40:48 +1200

    add changes

--- Week1-Activity4/bmi_calculator.py
+++ Week1-Activity4/bmi_calculator.py
@@ -1,45 +1,41 @@
 # BMI Calculator CLI Application
 
-weight = float(input("Enter your weight in kilograms: "))
-height = float(input("Enter your height in meters: "))
-
  # introducing class
 class BMICalculator:
-   print("=== BMI Calculator CLI ===")
-    
-   # Validate input
-   if weight <= 0 or height <= 0:
-    print("Weight and height must be greater than 0.")
+   
+    # Function to calculate BMI
+    def calculate_bmi(weight, height):
+     return weight / (height ** 2)
 
-
-# Function to calculate BMI
-def calculate_bmi(weight, height):
-    return weight / (height ** 2)
-
-# Function to specify BMI category
-def get_bmi_category(bmi):
-    if bmi < 18.5:
+    # Function to specify BMI category
+    def get_bmi_category(bmi):
+     if bmi < 18.5:
         return "Underweight"
-    elif bmi < 25:
+     elif bmi < 25:
         return "Normal weight"
-    elif bmi < 30:
+     elif bmi < 30:
         return "Overweight"
-    else:
+     else:
         return "Obese"
-
-# Calculate BMI
-bmi = calculate_bmi(weight, height)
-    
-# Get BMI category
-category = get_bmi_category(bmi)
-    
-# Display result
-print(f"\nYour BMI is: {bmi:.2f}")
-print(f"Category: {category}")    
-
-# Main function
-def main():
+  
 
 # Run the program
- if __name__ == "__main__":
-     main()
+if __name__ == "__main__":
+     print("=== BMI Calculator CLI ===")
+    
+     weight = float(input("Enter your weight in kilograms: "))
+     height = float(input("Enter your height in meters: "))
+         
+     # Validate input
+     if weight <= 0 or height <= 0:
+      print("Weight and height must be greater than 0.")
+      
+     # Calculate BMI
+     bmi = BMICalculator.calculate_bmi(weight, height)
+    
+     # Get BMI category
+     category = BMICalculator.get_bmi_category(bmi)
+    
+     # Display result
+     print(f"\nYour BMI is: {bmi:.2f}")
+     print(f"Category: {category}")   
